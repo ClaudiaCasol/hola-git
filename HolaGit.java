fb6384e8d5eb6bf9d04e42bd2b1d1175fb62df0a
ManelLlurda <manelllurda@alumnes.ilerna.com>
2025-11-06 16:54:08 +0100

    Agregado mensaje personal de Clau en HolaGit.java

--- HolaGit.java
+++ HolaGit.java
@@ -1,5 +1,6 @@
 public class HolaGit {
     public static void main(String[] args) {
         System.out.println("¡Hola Git!");
+        System.out.println("Mensaje de Clau en su rama personal.");
     }
 }
