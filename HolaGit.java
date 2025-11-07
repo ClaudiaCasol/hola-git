e73b48218a8947e8116dcb626bf6f6d29b51795c
ManelLlurda <manelllurda@alumnes.ilerna.com>
2025-11-07 16:08:38 +0100

    Cambio de mensaje en HolaGit por Manel

--- HolaGit.java
+++ HolaGit.java
@@ -1,6 +1,6 @@
 public class HolaGit {
     public static void main(String[] args) {
         System.out.println("¡Hola Git!");
-        System.out.println("Mensaje de Clau en su rama personal.");
+        System.out.println("Mensaje de Manel en su rama personal.");
     }
 }
