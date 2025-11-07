497716735e44941dfeb6ef5fa600e5f2cc8dae8c
ClaudiaCasol <claudiacasolxxi@alumnes.ilerna.com>
2025-11-07 16:27:07 +0100

    Resuelto conflicto al actualizar main

--- HolaGit.java
+++ HolaGit.java
@@ -2,5 +2,6 @@ public class HolaGit {
     public static void main(String[] args) {
         System.out.println("¡Hola Git!");
         System.out.println("Mensaje de Clau en su rama personal.");
+	System.out.println("Esto es un cambio en el código.");
     }
 }
